783b1a76f3961a3d9550e6c24e1c6d195f63b92f
Shashin Gounden <shashin.gounden@bbd.co.za>
2025-03-10 21:38:03 +0200

    Initial Pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,69 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    region  = "af-south-1"
+  }
+}
+
+provider "aws" {
+  region =  "af-south-1"
+}
+
+resource "aws_default_vpc" "default_vpc" {
+  tags = {
+    Name = "default_vpc"
+  }
+}
+
+data "aws_availability_zones" "available_zones" {
+  
+}
+
+resource "aws_default_subnet" "subnet_az1" {
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
+}
+
+resource "aws_default_subnet" "subnet_az2" {
+  availability_zone = data.aws_availability_zones.available_zones.names[1]
+}
+
+resource "aws_security_group" "allow_postgres" {
+  name_prefix = "allow_postgres_"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "fupboarddb" {
+  identifier             = "fupboarddb"
+  engine                 = "postgres"
+  engine_version         = "17.2"
+  instance_class         = "db.md5.large"
+  db_name                = "fupboarddb"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  publicly_accessible    = true
+  username               = var.db_username
+  password               = var.db_password
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.allow_postgres.id]
+  tags = {
+    Name = "fupboarddb"
+  }
+}
+
+output "db_host" {
+  value = aws_db_instance.beantradedb.endpoint
+  description = "The endpoint of the SQL Server RDS instance"
+}
