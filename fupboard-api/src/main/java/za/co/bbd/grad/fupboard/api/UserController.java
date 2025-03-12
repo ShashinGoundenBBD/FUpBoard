56d58d68347c16a9c99a5f103096b60c4d875d82
Luke Davis <luke.davis@sanlam.co.za>
2025-03-12 11:20:45 +0200

    Proper Error Handling on UserController

--- fupboard-api/src/main/java/za/co/bbd/grad/fupboard/api/UserController.java
+++ fupboard-api/src/main/java/za/co/bbd/grad/fupboard/api/UserController.java
@@ -3,14 +3,15 @@ package za.co.bbd.grad.fupboard.api;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.transaction.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class UserController {
@@ -23,26 +24,30 @@ public class UserController {
 	}
 
 	@PatchMapping("/v1/users/me")
-	@Transactional
 	public User patch_me(@AuthenticationPrincipal Jwt jwt, @RequestBody UserUpdate update) throws URISyntaxException, IOException, InterruptedException {
 		var user = userRepository.findByJwt(jwt).get();
 
 		update.setEmail(update.getEmail().toLowerCase());
 
-		if (update.getEmail() != null && !update.getEmail().equals(user.getEmail())) {
-			user.setEmail(update.getEmail());
-
-			var jwtEmail = jwt.getClaimAsString("email");
-			boolean jwtEmailVerified = jwt.getClaimAsBoolean("email_verified");
-
-			var verified = update.getEmail().equals(jwtEmail) && jwtEmailVerified;
-			user.setEmailVerified(verified);
+		try {
+			if (update.getEmail() != null && !update.getEmail().equals(user.getEmail())) {
+				user.setEmail(update.getEmail());
+	
+				var jwtEmail = jwt.getClaimAsString("email");
+				boolean jwtEmailVerified = jwt.getClaimAsBoolean("email_verified");
+	
+				var verified = update.getEmail().equals(jwtEmail) && jwtEmailVerified;
+				user.setEmailVerified(verified);
+			}
+			if (update.getUsername() != null && !update.getUsername().equals(user.getUsername())) {
+				user.setUsername(update.getUsername());
+			}
+			userRepository.save(user);
+		} catch (DataIntegrityViolationException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
 		}
-		if (update.getUsername() != null && !update.getUsername().equals(user.getUsername())) {
-			user.setUsername(update.getUsername());
-		}
-		userRepository.save(user);
-        return user;
+        
+		return user;
 	}
 }
 
