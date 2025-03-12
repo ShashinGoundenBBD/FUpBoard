b6fb3b4f6dd82c990f80ce732bc8d85461b36b7c
Luke Davis <luke.davis@sanlam.co.za>
2025-03-12 10:56:49 +0200

    Seed Data

--- migrations/V202503120847__SeedDefaultRole.sql
+++ migrations/V202503120847__SeedDefaultRole.sql
@@ -3,9 +3,23 @@ WITH "default_role_id" AS (
   INSERT INTO "roles" VALUES (DEFAULT, 'default') RETURNING "role_id"
 ), "read_jwt_permission_id" AS (
   INSERT INTO "permissions" VALUES (DEFAULT, 'jwt::read::mine') RETURNING "permission_id"
+), "read_user_permission_id" AS (
+  INSERT INTO "permissions" VALUES (DEFAULT, 'user::read::me') RETURNING "permission_id"
+), "write_user_permission_id" AS (
+  INSERT INTO "permissions" VALUES (DEFAULT, 'user::write::me') RETURNING "permission_id"
 )
 
 INSERT INTO "role_permissions" VALUES (
   (SELECT "role_id" FROM "default_role_id"),
   (SELECT "permission_id" FROM "read_jwt_permission_id")
+);
+
+INSERT INTO "role_permissions" VALUES (
+  (SELECT "role_id" FROM "default_role_id"),
+  (SELECT "permission_id" FROM "read_user_permission_id")
+);
+
+INSERT INTO "role_permissions" VALUES (
+  (SELECT "role_id" FROM "default_role_id"),
+  (SELECT "permission_id" FROM "write_user_permission_id")
 );
