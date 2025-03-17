0cf9aeb2b4fbfc18ef2c75f682762299dd58cd5a
Tshepo Ncube <Tshepo.Ncube@bbd.co.za>
2025-03-17 12:33:27 +0200

    commit

--- fupboard-api/src/main/java/za/co/bbd/grad/fupboard/api/models/UpdateFUpRequest.java
+++ fupboard-api/src/main/java/za/co/bbd/grad/fupboard/api/models/UpdateFUpRequest.java
@@ -2,4 +2,8 @@ package za.co.bbd.grad.fupboard.api.models;
 
 public class UpdateFUpRequest extends CreateFUpRequest {
 
+    public UpdateFUpRequest(String name, String description) {
+        super(name, description);
+    }
+
 }
